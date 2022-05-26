fd7293f50fcb2e31d05515c5a30becc76e53f1c8
Serihusna <2020862706@student.uitm.edu.my>
2022-05-26 12:22:05 +0800

    second commit

--- add_user.php
+++ add_user.php
@@ -9,7 +9,7 @@
 <body>
 <?php
 include 'conn.php' ;
-$sql ="insert into user (first_name,last_name,email,passwod)
+$sql ="insert into user (first_name,last_name,email,pass_word)
 values ('$_POST[firstname]','$_POST[lastname]','$_POST[email]','$_POST[password]','$_POST[reg_date]')";
 if (!mysqli_query($conn,$sql)){
     die ('Error:'.mysqli_error());
